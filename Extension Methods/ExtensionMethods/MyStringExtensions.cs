3838aa75d00491a38e54fcf096c43f44608f1315
rjlgithub <bigcats96@hotmail.com>
2014-06-09 09:54:37 -0600

    Extension Methods

---
+++ Extension Methods/ExtensionMethods/MyStringExtensions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    public static class MyStringExtensions
+    {
+        //Extension methods are static methods declared inside
+        //  static class
+        //The first parameter uses teh keyword "this" and references
+        //  the class or data type that we want to 'extend'.
+        public static string Quack(this string self)
+        {
+            return "<quack>" + self + "</quack>";
+        }
+
+        public static string Walk(this string self, int steps)
+        {
+            string result = self;
+
+            //Walk forwards or backwards...
+            if (steps < 0)
+            {
+                while (steps < 0)
+                {
+                    result = "[backstep] " + result;
+                    steps++;
+                }
+            }
+            else
+            {
+                while (steps > 0)
+                {
+                    result = result + " [step]";
+                    steps--;
+                }
+
+            }
+
+            return result;
+            return "<quack> " + self + " </quack>";
+        }
+     
+    }
+}
