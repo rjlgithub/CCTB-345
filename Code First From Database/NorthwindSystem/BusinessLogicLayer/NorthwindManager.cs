56a46066bd1d0525591cdccfe5beabff4510597f
rjlgithub <bigcats96@hotmail.com>
2014-06-10 14:18:40 -0600

    Code First From Database - modification

---
+++ Code First From Database/NorthwindSystem/BusinessLogicLayer/NorthwindManager.cs
@@ -0,0 +1,34 @@
+﻿using NorthwindSystem.DataAccessLayer;
+using NorthwindSystem.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthwindSystem.BusinessLogicLayer
+{
+    //This is the primary public access into the NorthwindSystem data
+    public class NorthwindManager
+    {
+        public List<Employee> GetEmployees()
+        {
+            using (var context = new NWContext())
+            {
+                var result = context.Employees;
+                return result.ToList();
+            }
+        }
+
+        //TODO: Create a method called GetOrders() that will return a list of Order objets from teh database.
+        public List<Order> GetOrders()
+        {
+            using (var context = new NWContext())
+            {
+                var result = context.Orders;
+                return result.ToList();
+            }
+        }
+
+    }
+}
